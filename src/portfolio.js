8b0c8ba92ece7c4393c290ece231ebf3cf5fc4f7
Shreyank Byadagi <shrunk7byadagi@gmail.com>
2021-06-30 01:19:31 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -279,21 +279,17 @@ const achievementSection = {
 
 const blogSection = {
   title: "Blogs",
-  subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  subtitle: "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
 
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+      url: "https://medium.com/@shrunk7byadagi/automatically-start-tomcat-on-instance-startup-reboot-in-amazon-ec2-ubuntu-instance-33849a9d9090",
+      title: "Automatically start Tomcat on instance startup/reboot in Amazon EC2-Instance",
+      description: "Start you tomcat automatically on instance re-boot"
     },
     {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+      url: "https://medium.com/the-angular-dark-secrets/lazy-loading-in-angular-7-vs-angular-8-62e3a6d5cbc1",
+      title: "Lazy Loading in Angular 7 Vs Angular 8"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -335,8 +331,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3243454077",
-  email_address: "saadpasta70@gmail.com"
+  number: "+91-9972423247",
+  email_address: "shrunk7byadagi@gmail.com"
 };
 
 // Twitter Section
